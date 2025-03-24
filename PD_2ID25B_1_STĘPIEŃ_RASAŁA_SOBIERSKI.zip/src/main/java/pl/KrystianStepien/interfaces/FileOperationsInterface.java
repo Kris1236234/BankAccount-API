d5aaca6448ef03725622ee207dd888729fab4c4e
Krystian <krystina.stepien4@o2.pl>
2025-03-24 22:52:56 +0100

    Ponowna inicjalizacja repozytorium

--- PD_2ID25B_1_STĘPIEŃ_RASAŁA_SOBIERSKI.zip/src/main/java/pl/KrystianStepien/interfaces/FileOperationsInterface.java
+++
@@ -1,8 +0,0 @@
-package pl.KrystianStepien.interfaces;
-
-import pl.KrystianStepien.model.BankAccount;
-
-public interface FileOperationsInterface {
-    void saveAccount(BankAccount account);
-    BankAccount loadAccount(String pesel);
-}
--- PD_2ID25B_1_STĘPIEŃ_RASAŁA_SOBIERSKI.zip/src/main/java/pl/KrystianStepien/interfaces/JsonSerializable.java
+++
@@ -1,5 +0,0 @@
-package pl.KrystianStepien.interfaces;
-
-public interface JsonSerializable {
-    String toJson();
-}
